d03de571b7fc93807fbb170ac7b9ef74c0de0204
Mico Febrian <mico.febrian@orderfaaz.com>
2024-11-14 21:17:37 +0700

    💄 update style for layouts

---
+++ components/layouts/AuthLayout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import MainLayout from "./MainLayout";
+
+type AuthLayoutProps = {
+  children: React.ReactNode;
+};
+
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  return (
+    <MainLayout>
+      <div className="bg-auth-radial h-full pt-40 px-5">{children}</div>
+    </MainLayout>
+  );
+};
+
+export default AuthLayout;
