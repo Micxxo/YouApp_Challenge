c8dd0221d1a9cf2bbf58300f4e8981bb7e0f8144
Mico Febrian <mico.febrian@orderfaaz.com>
2024-11-16 21:45:47 +0700

    🐛 Fix build erorr

--- app/interest/layout.tsx
+++ app/interest/layout.tsx
@@ -1,40 +1,16 @@
 import InterestLayout from "@/components/layouts/InterestLayout";
-import { Button } from "@/components/ui/button";
-import { Icon } from "@iconify/react/dist/iconify.js";
-import Link from "next/link";
 import React from "react";
 
-interface LayoutProps {
+type LayoutProps = {
   children: React.ReactNode;
-  onSave: () => void;
-}
+};
 
-const Layout: React.FC<LayoutProps> = ({ children, onSave }) => {
+const layout = ({ children }: LayoutProps) => {
   return (
     <InterestLayout>
-      <div className="pt-10 px-6 text-white">
-        <div className="flex items-center justify-between w-full">
-          <Link href={"/"}>
-            <Button className="!bg-transparent !w-fit !h-fit !p-0 flex items-center gap-1">
-              <Icon
-                icon={"ion:chevron-back"}
-                fontSize={28}
-                fontWeight={"bold"}
-              />
-              <p className="text-sm font-bold">Back</p>
-            </Button>
-          </Link>
-          <Button
-            onClick={onSave}
-            className="!bg-transparent !w-fit !h-fit !p-0"
-          >
-            <p className="text-sm font-semibold gradient-blue-text">Save</p>
-          </Button>
-        </div>
-        {children}
-      </div>
+      <div className="pt-10 px-6 text-white">{children}</div>
     </InterestLayout>
   );
 };
 
-export default Layout;
+export default layout;
--- components/templates/interest/ManageInterestPage.tsx
+++ components/templates/interest/ManageInterestPage.tsx
@@ -1,32 +1,51 @@
 "use client";
-import GlassBadge from "@/components/molecules/glassBadge";
+import GlassBadge from "@/components/molecules/GlassBadge";
+import { Button } from "@/components/ui/button";
+// import GlassBadge from "@/components/molecules/glassBadge";
 import { Input } from "@/components/ui/input";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import Link from "next/link";
 import React from "react";
 
 const ManageInterestPage = () => {
   return (
-    <div className="pt-20">
-      <h1 className="text-sm font-bold gradient-golden-text">
-        Tell everyone about yourself
-      </h1>
-      <h1 className="font-bold text-xl mt-2 text-white">What interest you?</h1>
-      <div className="mt-8 w-full bg-[#D9D9D90F] rounded-[10px] px-4 py-3">
-        <div className="flex flex-wrap gap-1">
-          <GlassBadge onDelete={() => console.log("hi")} title="Music" />
-          <GlassBadge onDelete={() => console.log("hi")} title="Music" />
-          <GlassBadge onDelete={() => console.log("hi")} title="Music" />
-          <GlassBadge onDelete={() => console.log("hi")} title="Music" />
+    <>
+      <div className="flex items-center justify-between w-full">
+        <Link href={"/"}>
+          <Button className="!bg-transparent !w-fit !h-fit !p-0 flex items-center gap-1">
+            <Icon icon={"ion:chevron-back"} fontSize={28} fontWeight={"bold"} />
+            <p className="text-sm font-bold">Back</p>
+          </Button>
+        </Link>
+        <Button className="!bg-transparent !w-fit !h-fit !p-0">
+          <p className="text-sm font-semibold gradient-blue-text">Save</p>
+        </Button>
+      </div>
+      <div className="pt-20">
+        <h1 className="text-sm font-bold gradient-golden-text">
+          Tell everyone about yourself
+        </h1>
+        <h1 className="font-bold text-xl mt-2 text-white">
+          What interest you?
+        </h1>
+        <div className="mt-8 w-full bg-[#D9D9D90F] rounded-[10px] px-4 py-3">
+          <div className="flex flex-wrap gap-1">
+            <GlassBadge onDelete={() => console.log("hi")} title="Music" />
+            <GlassBadge onDelete={() => console.log("hi")} title="Music" />
+            <GlassBadge onDelete={() => console.log("hi")} title="Music" />
+            <GlassBadge onDelete={() => console.log("hi")} title="Music" />
+          </div>
+          <Input
+            variant="transparent"
+            className="!py-2 !w-full focus-visible:ring-0 focus-visible:ring-offset-0 mt-1"
+            autoFocus
+          />
         </div>
-        <Input
-          variant="transparent"
-          className="!py-2 !w-full focus-visible:ring-0 focus-visible:ring-offset-0 mt-1"
-          autoFocus
-        />
+        <div className="w-full bg-[#D9D9D90F] hover:bg-white/15 duration-200 rounded-[10px] px-4 py-3 mt-3 cursor-pointer">
+          <h1 className="text-sm text-white font-semibold">Music</h1>
+        </div>
       </div>
-      <div className="w-full bg-[#D9D9D90F] hover:bg-white/15 duration-200 rounded-[10px] px-4 py-3 mt-3 cursor-pointer">
-        <h1 className="text-sm text-white font-semibold">Music</h1>
-      </div>
-    </div>
+    </>
   );
 };
 
