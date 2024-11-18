25843fd7eca46e9da42b962d71731572a4be5506
Mico Febrian <mico.febrian@orderfaaz.com>
2024-11-18 11:11:17 +0700

    🐛 store the response on hooks

--- services/hooks/auth/login.ts
+++ services/hooks/auth/login.ts
@@ -12,5 +12,6 @@ export const loginHooks = async ({ keyword, password }: loginProps) => {
     password: password,
   });
 
+  localStorage.setItem("tes", JSON.stringify(signInPromise));
   return signInPromise;
 };
